4e1ac2ac66b7012c7c6c2cb0c395b21428daed57
Burak <burakguler@users.noreply.github.com>
2020-11-11 23:20:15 +0300

    executing query

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
         DbHelper helper = new DbHelper();
+        Statement statement = null;
+        ResultSet resultSet;
         try{
             connection = helper.getConnection();
-            System.out.println("Bağlantı oluştu!");
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select Code,Name,Continent,Region from country");
+            while ((resultSet.next())){
+                System.out.println(resultSet.getString("Name"));
+            }
         }catch (SQLException exception) {
             helper.showErrorMessage(exception);
         }
