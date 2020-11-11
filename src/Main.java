818bd05a406df3b7c76421852f84373e416bf235
Burak <burakguler@users.noreply.github.com>
2020-11-11 22:29:10 +0300

    defining connection parameters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
 
+    static String userName="root";
+    static String password="12345";
+    static String dbUrl="jdbc:mysql://localhost:3306/world";
+
     public static void main(String[] args) {
 	// write your code here
     }
