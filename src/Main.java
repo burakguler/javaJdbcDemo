9cab206d3cb238801307cad9f27e7de4a85f3f1c
Burak <burakguler@users.noreply.github.com>
2020-11-11 23:31:57 +0300

    getting result to class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -11,9 +12,15 @@ public class Main {
             connection = helper.getConnection();
             statement = connection.createStatement();
             resultSet = statement.executeQuery("select Code,Name,Continent,Region from country");
+            ArrayList<Country> countries = new ArrayList<Country>();
             while ((resultSet.next())){
-                System.out.println(resultSet.getString("Name"));
+                countries.add(new Country(
+                        resultSet.getString("Code"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Continent"),
+                        resultSet.getString("Region")));
             }
+            System.out.println(countries.size());
         }catch (SQLException exception) {
             helper.showErrorMessage(exception);
         }
